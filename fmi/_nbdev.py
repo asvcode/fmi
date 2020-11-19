66ff0e73f60e80a1e944f0aed1b3f89ab940a594
asvcode <avirdee@aol.com>
2020-11-19 10:16:30 -0800

    initial_core

--- fmi/_nbdev.py
+++ fmi/_nbdev.py
@@ -2,9 +2,16 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"instance_sort": "00_core.ipynb",
+         "instance_dcmread": "00_core.ipynb",
+         "instance_show": "00_core.ipynb",
+         "get_dicom_image": "00_core.ipynb",
+         "mask_and_save": "00_core.ipynb",
+         "DcmDataset.updated_dict": "00_core.ipynb",
+         "pd.DataFrame.from_dicoms2": "00_core.ipynb",
+         "dicom_convert_3channel": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://fastai.github.io/fmi/"
 
